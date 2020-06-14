73bbc2250b41b143e58b379e653c0e0c5e119e13
debuggerboy <debuggerboy@gmail.com>
2020-06-14 18:40:58 +0530

    Corrections

--- storage/docker-pynode/data/uwsgi.sh
+++ storage/docker-pynode/data/uwsgi.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-#apk update
-#apk add gcc g++ linux-headers
+if [ ! -d "/data/htdocs/local_docker_python_node/venv" ]
+then
+    echo "Python3 Virtual Environment does not Exists"
+    # We need to create Python3 Virtual Environment.
+    # In that case we will require installing 2 alpine packages
+    apk update
+    apk add gcc g++ linux-headers
+    # Creating the Python3 Virtual Environment.
+    python -m venv /data/htdocs/local_docker_python_node/venv
+    # Installing the Python Flask and uWSGI modules
+    pip install Flask uWSGI
+    echo "Done Setting up Python3 Virtual Environment."
+fi
 
 cd /data/
 source ./htdocs/local_docker_python_node/venv/bin/activate
